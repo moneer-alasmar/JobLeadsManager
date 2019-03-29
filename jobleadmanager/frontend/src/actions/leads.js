ced09dbfe38ee6109afd0ae786dcf40a82cbc4da
Moneer <kiraalasmar@gmail.com>
2019-03-28 17:46:39 -0700

    began django token authentication logic

--- jobleadmanager/frontend/src/actions/leads.js
+++ jobleadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from "./types";
-import { createMessage } from "./messages";
+import { createMessage, returnErrors } from "./messages";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
 // GET LEADS
 export const getLeads = () => dispatch => {
@@ -11,7 +11,9 @@ export const getLeads = () => dispatch => {
         payload: res.data
       });
     })
-    .catch(err => console.log(err));
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
 // DELETE LEAD
@@ -37,14 +39,7 @@ export const addLead = lead => dispatch => {
         payload: res.data
       });
     })
-    .catch(err => {
-      const errors = {
-        message: err.response.data,
-        status: err.response.status
-      };
-      dispatch({
-        type: GET_ERRORS,
-        payload: errors
-      });
-    });
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
